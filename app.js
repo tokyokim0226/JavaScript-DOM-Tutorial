8388dd1b5b0ee96dfe0c6d2eb104e9520daeabde
Seongmoon Kim <tokyokim0226@naver.com>
2023-12-22 14:44:14 +0900

    lesson 15 done

--- app.js
+++ app.js
@@ -34,12 +34,21 @@ addForm.addEventListener('submit', function(e){
   list.appendChild(li);
 });
 
-// hide books
+// // hide books
+// const hideBox = document.querySelector('#hide');
+// hideBox.addEventListener('change', function(e){
+//   if(hideBox.checked){
+//     list.style.display = "none";
+//   } else {
+//     list.style.display = "initial";
+//   }
+// });
+
 const hideBox = document.querySelector('#hide');
-hideBox.addEventListener('change', function(e){
-  if(hideBox.checked){
-    list.style.display = "none";
-  } else {
-    list.style.display = "initial";
-  }
+hideBox.addEventListener('change', function (e) {
+    if (hideBox.checked){
+        list.style.display = "none";
+    }else {
+        list.style.display = "initial";
+    }
 });
